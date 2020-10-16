226e8b060e1fd6979388cf4ee3cbc65a5cee1229
SYJINTW <yuanjiun911@outlook.com>
2020-10-16 17:18:22 +0800

    add binary_tree

--- Week_5/binary_tree.c
+++ Week_5/binary_tree.c
@@ -13,6 +13,8 @@ Node* constructTree(int inorder[], int preorder[], int inorder_start, int inorde
 void inorder(Node *root);
 void preorder(Node *root);
 void destroy(Node* root);
+int maxValue(Node* root);
+void writeGV(Node *tree);
 
 //找到 inorder 符合 value 的位置
 int idxSearch(int arr[], int start, int end, int value){
@@ -37,9 +39,9 @@ Node* newNode(int val){
 }
 
 Node* constructTree(int inorder[], int preorder[], int inorder_start, int inorder_end){
-	//Way 1
-	/*
- 	static int preorder_idx = 0;
+	static int preorder_idx = 0;
+	int i;
+
  	if(inorder_start > inorder_end) return NULL;
  	else;
 
@@ -49,11 +51,10 @@ Node* constructTree(int inorder[], int preorder[], int inorder_start, int inorde
  	if(inorder_start == inorder_end) return tree_node;
  	else;
 
- 	int i = idxSearch(inorder, inorder_start, inorder_end, tree_node->data);
+ 	i = idxSearch(inorder, inorder_start, inorder_end, tree_node->data);
  	tree_node->left = constructTree(inorder, preorder, inorder_start, i-1);
  	tree_node->right = constructTree(inorder, preorder, i+1, inorder_end);
- 	*/
- 	
+
  	return tree_node;
 }
 
@@ -73,7 +74,53 @@ void preorder(Node *root){
 	}
 }
 
+int maxValue(Node* root){
+	if(root != NULL){
+		int max = root->data;
+		int value;
+		if(root->left != NULL){
+			value = maxValue(root->left);
+			if(value > max) max = value;
+			else;
+		} else;
 
+		if(root->right != NULL){
+			value = maxValue(root->right);
+			if(value > max) max = value;
+			else;
+		} else;
+
+		return max;
+	}
+	else return -1;
+}
+
+void destroy(Node* root){
+	if(root != NULL){
+		destroy(root->left);
+		destroy(root->right);
+		free(root);
+	} else;
+}
+
+//Output tree.gv for graphviz
+void printTree(FILE *fout, Node *tree){
+    if (tree!=NULL) {
+        if (tree->left!=NULL)
+            fprintf(fout, "%d -> %d;\n", tree->data, tree->left->data);
+        if (tree->right!=NULL)
+            fprintf(fout, "%d -> %d;\n", tree->data, tree->right->data);
+        printTree(fout, tree->left);
+        printTree(fout, tree->right);
+    }
+}
+
+void writeGV(Node *tree){
+    FILE *fout = fopen("tree.gv", "w");
+    fprintf(fout, "digraph T {\n");
+    printTree(fout, tree);
+    fprintf(fout, "}\n");
+}
 
 int main(){
 	//Construct a Tree by inorder_list and preorder_list
@@ -88,9 +135,21 @@ int main(){
 		scanf("%d", pre+i);
 	}
 	Node* root = constructTree(in, pre, 0, n-1);
+	
+	//Output for graphviz
+	writeGV(root);
+
+	//Output tree
 	inorder(root);
 	printf("\n");
 	preorder(root);
+	printf("\n");
+	printf("%d\n", maxValue(root));
+
+	//Clean memory
+	destroy(root);
+	free(in);
+	free(pre);
 
 	//Create a Tree by hand
 	/*
