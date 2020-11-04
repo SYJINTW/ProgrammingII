be93b74a0fc28fdba0e094467e424648ffd6584f
SYJINTW <yuanjiun911@outlook.com>
2020-11-05 01:42:25 +0800

    Midterm and PDF

--- Midterm/12254.c
+++ Midterm/12254.c
@@ -3,40 +3,46 @@
 
 #define NUM 1000000007
 
-int matrix[3][3] = {{1, 2, 1}, {1, 0, 0}, {0, 1, 0}};
-int tmp[3][3];
-int var[3] = {1, 12, 13};
-int t_var[3];
+unsigned long long matrix[3][3];
+unsigned long long t_matrix[3][3];
+unsigned long long var[3];
+unsigned long long t_var[3];
 
 void matrix_mul(){
-	int sum;
-	//row
-	for(int i = 0; i < 3; i++){
-		//col
-		for(int k = 0; k < 3; k++){
-			sum = 0;
-			for(int j = 0; j < 3; j++){
-				sum = sum + (matrix[i][j]*matrix[j][k])%NUM;
-			}
-			tmp[i][k] = sum;
-		}
-	}
+	//initial t_matrix
 	for(int i = 0; i < 3; i++){
 		for(int j = 0; j < 3; j++){
-			matrix[i][j] = tmp[i][j];
+			t_matrix[i][j] = 0;
+		}
+	}
+	//calculate
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			for(int k = 0; k < 3; k++){
+				t_matrix[i][j] = (t_matrix[i][j] + (matrix[i][k] * matrix[k][j])%NUM)%NUM;
+			}
+		}
+	}
+	//record
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			matrix[i][j] = t_matrix[i][j];
 		}
 	}
 }
 
 void matrix_var(){
-	int sum;
+	//initial t_var
 	for(int i = 0; i < 3; i++){
-		sum = 0;
-		for(int j = 0; j < 3; j++){
-			sum = sum + (matrix[i][j]*var[j])%NUM;
-		}
-		t_var[i] = sum;
+		t_var[i] = 0;
 	}
+	//calculate
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			t_var[i] = (t_var[i] + (matrix[i][j] * var[j])%NUM)%NUM;
+		}
+	}
+	//record
 	for(int i = 0; i < 3; i++){
 		var[i] = t_var[i];
 	}
@@ -48,16 +54,17 @@ int main(){
 	scanf("%lld", &t);
 	while(t--){
 		scanf("%lld", &x);
-		if(x == 1){
-			ans = 1;
-		}
-		else if(x == 2){
-			ans = 12;
-		}
-		else if(x == 3){
-			ans = 13;
-		}
+		//initial
+		matrix[1][1] = matrix[1][2] = matrix[2][0] = matrix[2][2] = 0;
+		matrix[0][0] = matrix[0][2] = matrix[1][0] = matrix[2][1] = 1;
+		matrix[0][1] = 2;
+		var[0] = 13; var[1] = 12; var[2] = 1;
+
+		if(x == 1) ans = var[2];
+		else if(x == 2) ans = var[1];
+		else if(x == 3) ans = var[0];
 		else{
+			x = x - 3;
 			while(x){
 				if(x%2 == 0){
 					matrix_mul();
@@ -72,6 +79,25 @@ int main(){
 		}
 		printf("%lld\n", ans);
 	}
+	
+	/*
+	matrix_mul();
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			printf("%lld ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	matrix_mul();
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			printf("%lld ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	*/
+
+	return 0;
 }	
 
 
---
+++ Midterm/12435.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ul long long int
+#define NUM 1000000007
+
+//define struct
+typedef struct{
+	ul time;
+	ul value;
+} arr_struct;
+
+//define variables
+ul n, q, tmp;
+arr_struct arr[1000009]; //start from 1 to n
+
+
+ul binarySearch(ul x){
+	ul l = 1, r = n, mid;
+	ul ans = 1; //ans is last time mid (for when the last time is found)
+	while(l < r){
+		mid = (r+l)/2;
+		//find left
+		if(arr[mid].value >= x){
+			ans = mid;
+			r = mid;
+		}
+		//find right
+		else{
+			l = mid+1;
+		}
+	}
+	//find the ans (l == r)
+	if(arr[ans].value == x){
+		return arr[ans].time;
+	}
+	//didn't find the ans (l < r)
+	else{
+		return 0;
+	}
+}
+
+ul fpow(ul k){
+	ul base = 2;
+	ul ans = 1;
+	while(k){
+		/*
+		if(k&1){
+			ans = ((ans)%NUM * (base)%NUM) % NUM;
+		}
+		base = ((base)%NUM * (base)%NUM) % NUM;
+		k = k >> 1;
+		*/
+		if(k % 2 == 0){
+			base = ((base)%NUM * (base)%NUM) % NUM;
+			k = k/2;
+		}
+		else{
+			ans = ((ans)%NUM * (base)%NUM) % NUM;
+			k--;
+		}
+	}
+	return ans;
+}
+
+
+/*** 背起來 ***/
+//for qsort() 
+int cmp(const void * a, const void * b){
+   return (*(arr_struct*)a).value - (*(arr_struct*)b).value;
+}
+
+int main(){
+	while(scanf("%lld %lld", &n, &q) != EOF){
+		for(int i = 0; i < 1000009; i++){
+			arr[i].time = -1;
+			arr[i].value = -1;
+		}
+
+		for(int i = 1; i <= n; i++){
+			scanf("%lld(/`A`)/ ~I__I", &tmp);
+			if(tmp % 2 == 0){
+				arr[i].value = fpow(tmp/2);
+				arr[i].time = i;
+			}
+			else{
+				arr[i].value = 0;
+				arr[i].time = i;
+			}
+		}
+
+		qsort(arr+1, n, sizeof(arr_struct), cmp);
+
+		while(q--){
+			ul x, ans;
+			scanf("%lld", &x);
+			ans = binarySearch(x);
+			if(ans == 0){
+				printf("Go Down Chicken 404\n");
+			}
+			else{
+				printf("%lld\n", ans);
+			}
+		}
+	}
+	return 0;
+}
