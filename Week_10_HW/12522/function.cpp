0fea36515624c2e4dc2dc2994380eb373a0f92f2
SYJINTW <yuanjiun911@outlook.com>
2020-12-01 21:13:11 +0800

    Week_10_HW finish

--- Week_10_HW/12522/function.cpp
+++ Week_10_HW/12522/function.cpp
@@ -17,14 +17,17 @@ Matrix Matrix::operator+ (const Matrix &x) const{
 	Matrix result(row, col);
 	for(int i = 0; i < row; i++){
 		for(int j = 0; j < col; j++){
+			// 這裡的 mat 代表 this 中的矩陣
 			result[i][j] = ((mat[i][j] + x[i][j]) % MOD + MOD) % MOD;
 		}
 	}
 	return result;
 }
 
+// 因為是 friend 形式，所以可以用外部函數的方式去完成它
 Matrix operator* (const Matrix &x, const Matrix &y){
 	Matrix result(x.row, y.col);
+	// 基本的矩陣相乘
 	for(int i = 0; i < x.row; i++){
 		for(int j = 0; j < y.col; j++){
 			for(int k = 0; k < x.col; k++){
